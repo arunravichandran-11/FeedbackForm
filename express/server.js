14f832c380110ba02b55a749f1b4eaa7f1c60da8
arunravichandran-11 <arunravichandran11@gmail.com>
2021-02-28 20:46:49 +0530

    asset build folder is added

--- express/server.js
+++ express/server.js
@@ -82,7 +82,7 @@ router.post('/answers', (req, res) => {
    res.status(200).send({success: true});
 });
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, 'build'));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
