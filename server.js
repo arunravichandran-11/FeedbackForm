1d35a2f9bf89f30602442fadb9e388ffeffac362
arunravichandran-11 <arunravichandran11@gmail.com>
2021-02-28 13:22:06 +0530

    ReadMe updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 8080;
+const port = 9000;
 const app = express();
 app.use(express.static(__dirname));
 
